2cbce1698e8ab52ef109b055b876b54ec9d87c6e
erdieee <58039191+erdieee@users.noreply.github.com>
2023-07-10 16:21:14 +0200

    imporve home page

--- analyzef1/Home.py
+++ analyzef1/Home.py
@@ -20,12 +20,27 @@ logging.basicConfig(level=logging.INFO, format=LOGFORMAT)
 def main() -> None:
     set_page_config()
     logger.info("Showing Home Page")
+    st.title("Analyze Formula 1 🏎️")
+
     col1, col2 = st.columns([2, 2])
     col1.image(f"{Path().resolve()}/images/background/backgroundcrop.jpeg")
-    col2.title("Analyze F1 🏎️")
-    col2.write("For all the F1 nerds.")
-    st.markdown("---")
-    st.write("Just a math student enjoying data")
+    col2.markdown("For all the **F1** nerds.")
+    col2.markdown("Just a math student enjoying data")
+    st.divider()
+    st.subheader("Event Schedule")
+    st.markdown(
+        f"Display current race season. Next, upcoming and past events with session times."
+    )
+    st.subheader("Analyze Session")
+    st.markdown(
+        f"Analyze all previous sessions up to **2018**. If the session has just ended, the data should be available a few minutes later."
+    )
+    st.subheader("Leaderboard")
+    st.markdown(f"Driver and constructor standings up to **2005**.")
+    st.divider()
+    st.markdown(
+        f"In case you find any bugs or want to improve the page, make sure to open an issue or pull request on [Github](https://github.com/erdieee/analyzef1)."
+    )
 
 
 if __name__ == "__main__":
